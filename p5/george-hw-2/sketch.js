893897f2534e65cd30b2ff4a6c0c5df8f25a1856
George Shoemaker <george.a.shoemaker@gmail.com>
2020-10-06 22:07:54 -0600

    sound

--- p5/george-hw-2/sketch.js
+++ p5/george-hw-2/sketch.js
@@ -37,30 +37,45 @@ class JugglePattern {
 
 const winWidth = 800
 const winHeight = 450
+const fps = 60
 
 const h = 5;
 const duration = 30;
 const tosses = JuggleToss.getTosses(h, duration)
-const jugglePattern = new JugglePattern([3, 3, 3]);
+const jugglePattern = new JugglePattern([1,2,3,5,3,6]);
 
 const xSpacer = 30
 const xAnchor = winWidth / 2 - xSpacer * jugglePattern.sequence.length / 2
 const yAnchor = 430
 
+const osc = new p5.Oscillator('sine');
+console.log(osc == null)
+
 let balls = JuggleBall.getBalls(
     xAnchor, xSpacer, yAnchor,
     jugglePattern.sequence.length,
     h, duration);
-for (let ball of balls) {
-    console.log(ball.toss.func == null)
-    ball.onComplete = () => {
-        console.log(ball.toss.func)
-        ball.toss = tosses[jugglePattern.next()]
+
+let oscs = [196, 220, 247, 262, 294, 330, 349, 392, 440, 494, 523].map(
+    hertz => new p5.Oscillator(hertz, 'sine')
+)
+
+let decay = (duration-1)/fps
+for (let i=0; i<balls.length; i++) {
+    balls[i].onComplete = () => {
+        balls[i].toss = tosses[jugglePattern.next()]
+        oscs[i].start()
+        oscs[i].amp(1)
+        oscs[i].amp(0,decay)
+        oscs[i].stop(0.5)
     }
 }
 
 function setup() {
     stroke(0);
+    frameRate(frameRate);
+   
+    console.log(osc == null)
     const canvas = createCanvas(winWidth, winHeight)
     canvas.parent('sketch-holder');
     canvas.style("display", "block");
