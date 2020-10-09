c21372e2b6027dc498660d12a2db846607772326
George Shoemaker <george.a.shoemaker@gmail.com>
2020-10-08 22:43:29 -0600

    ohhhh yeah

--- p5/george-hw-2/sketch.js
+++ p5/george-hw-2/sketch.js
@@ -44,86 +44,131 @@ class JugglePattern {
     }
 }
 
+function parseInput(str) {
+    if (str == null || str.length == 0) return null
+    let filtered = str.replace(/[^1-9]/gi, '')
+    if (filtered.length > 10) filtered = filtered.substring(0,10)
+    return filtered
+}
+
 const winWidth = 800
 const winHeight = 450
 const fps = 30
 
-const h = 4;
-const duration = 14;
+const h = 4;         //   height of the lowest toss
+const duration = 15; // duration of the lowest toss
 const tosses = JuggleToss.getTosses(h, duration)
-const jugglePattern = new JugglePattern([1,5,6,2,3,1,4,9]);
+let jugglePattern; //= new JugglePattern([9]);
 
 const ballWidth = 52
 const xSpacer = 76
-const xAnchor = winWidth / 2 - xSpacer * jugglePattern.sequence.length / 2 + ballWidth*3/4
+let xAnchor;// = winWidth / 2 - xSpacer * jugglePattern.sequence.length / 2 + ballWidth*3/4
 const yAnchor = 410
 
-const osc = new p5.Oscillator('sine');
-console.log(osc == null)
+let balls;
 
-let balls = JuggleBall.getBalls(
-    xAnchor, xSpacer, yAnchor,
-    jugglePattern.sequence.length,
-    h, duration, ballWidth);
+
 
 //            C        F          A
-let oscs = [ 261.626,  349.228,  440,
-             523.251,  698.456,  880,
-            1046.502, 1396.913, 1760,
-            2093.005].map(
-                 hertz => new p5.Oscillator(hertz, 'sine')
-             )
+let notes = [ 261.626,  349.228,  440,
+              523.251,  698.456,  880,
+             1046.502, 1396.913, 1760,
+             2093.005 ]
 
-let decay = (duration-1)/fps
-for (let i=0; i<balls.length; i++) {
-    balls[i].onComplete = () => {
-        balls[i].toss = tosses[jugglePattern.next()]
-        oscs[i].start()
-        oscs[i].amp(0)
-        oscs[i].amp(0.2,0.05)
-        oscs[i].amp(0,decay)
-        oscs[i].stop(0.5)
-        balls[i].colorScalar = 0;
-    }
-}
+let oscs = notes.map( hertz => new p5.Oscillator(hertz, 'sine') )
 
-let sequenceString = 'Sequence: [' + jugglePattern.sequence.join(', ') + ']'
+const decay = (duration-1)/fps
 
-// let button;
+let sequenceString //= '=> Sequence: [' + jugglePattern.sequence.join(', ') + ']'
 
 function setup() {
     stroke(0);
     frameRate(fps);
     console.log(userStartAudio == null)
-    userStartAudio();
-   
-    console.log(osc == null)
+
+    jugglePattern = new JugglePattern([9]);
+    xAnchor = winWidth / 2 - xSpacer * jugglePattern.sequence.length / 2 + ballWidth*3/4
+    
+    balls = JuggleBall.getBalls(
+        xAnchor, xSpacer, yAnchor,
+        jugglePattern.sequence.length,
+        h, duration, ballWidth
+    );
+
+    for (let i=0; i<balls.length; i++) {
+        balls[i].onComplete = () => {
+    
+            // Not sure why but I perceive higher notes to be louder
+            // This is my attempt to normalize that
+            const maxAmp = 0.2 - (notes[i]-notes[0])/8000
+    
+            balls[i].toss = tosses[jugglePattern.next()]
+            oscs[i].start()
+            oscs[i].amp(0)
+            oscs[i].amp(maxAmp,0.05)
+            oscs[i].amp(0,decay)
+            oscs[i].stop(0.5)
+            balls[i].colorScalar = 0;
+        }
+    }
+
     const canvas = createCanvas(winWidth, winHeight)
     canvas.parent('sketch-holder');
     canvas.style("display", "block");
+    
+    let canvasPosition = _renderer.position()
+        // This is a hack
+        // For whatever reason, the button / input position coords are relative to the DOM, not the canvas
 
-    // button = createButton('go');
-    // // button.position(_renderer.x, _renderer.y);
+    button = createButton('submit');
+    button.position(canvasPosition.x + 184, canvasPosition.y +30);
+    input = createInput('');
+    input.position(canvasPosition.x + 30, canvasPosition.y+30);
 
-    // let pos = _renderer.position()
-    // button.position(pos.x, pos.y);
-    // button.mousePressed(()=>console.log("yeet!"));
+    button.mousePressed( () => {
+        let parsed = parseInput(input.value())
+        if (parsed == null) return
+        let numberArray = parsed.split('').map( c => parseInt(c) )
+        jugglePattern = new JugglePattern(numberArray)
+        xAnchor = winWidth / 2 - xSpacer * jugglePattern.sequence.length / 2 + ballWidth*3/4
 
-    // console.log(pos.x)
-    // console.log(pos.y)
+        balls = JuggleBall.getBalls(
+            xAnchor, xSpacer, yAnchor,
+            jugglePattern.sequence.length,
+            h, duration, ballWidth
+        );
+    
+        for (let i=0; i<balls.length; i++) {
+            balls[i].onComplete = () => {
+        
+                // Not sure why but I perceive higher notes to be louder
+                // This is my attempt to normalize that
+                const maxAmp = (1 -(notes[i]/2000)) * 0.8//- (notes[i]-notes[0])/20000
+        
+                balls[i].toss = tosses[jugglePattern.next()]
+                oscs[i].start()
+                oscs[i].amp(0)
+                oscs[i].amp(maxAmp,0.05)
+                oscs[i].amp(0,decay)
+                oscs[i].stop(0.5)
+                balls[i].colorScalar = 0;
+            }
+        }
 
-    for (that in this) console.log(that)
+        sequenceString = numberArray.join(', ')
+    });
+    textSize(16);
 }
 
 function draw() {
     background(20)
     fill(255);
     noStroke();
-    text(sequenceString, 10, 20)
+    text("=>  [" + sequenceString + "]", 228, 25)
     balls.forEach(ball => ball.draw())
     stroke(150,0,0)
 }
 
 function mousePressed() {
     userStartAudio();
-  }
+}
